d65d03f7b5e8bb1436eba06e696e98009564acdd
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-12-11 20:48:30 -0500

    subindo arquivo. . .

---
+++ aula-jsx-tsx/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+const name = "Jaqueline";
+  return ( 
+    <>
+  <h1>hello, {name}!</h1>
+  <p>Primeiro teste</p>
+  </> // <--fragmento
+  );
+}
+
+export default App
