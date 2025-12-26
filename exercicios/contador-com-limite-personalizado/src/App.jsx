81b94bdc3289b0b5553b57ac6161fd59c6202d81
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-12-26 11:38:30 -0500

    feat: correção do exercício + ajustes no minha solução. Bom dia quinta-feirAAAAA

--- exercicios/contador-com-limite-personalizado/src/App.jsx
+++ exercicios/contador-com-limite-personalizado/src/App.jsx
@@ -8,32 +8,42 @@ function App() {
   const increment = () => {
     if  (initialValue < valueMax) {
       setinitialValue(prev => prev + 1); 
-      }  else {
-        alert("Valor máximo atingido!");
-      }
+      }  
     }
   const decrement = () => {
     if (initialValue > valueMax * -1) {
     setinitialValue(prev => prev - 1);
-    } else {
-      alert("Valor mínimo atingido!")
-    }
+    } 
 }
 
   return (
-    <>
+    <div style={{backgroundColor: "orange"}}>
     <h3>Contador com limite: {valueMax}</h3>
     <p>Valor atual: {initialValue}</p>
     <button onClick={increment}>+</button>
     <button onClick={decrement}>-</button>
+
+    {initialValue >= valueMax && (
+      <p style={{color: "red", fontWeight: "bold"}}>
+        Atenção: Você atingiu o limite máximo de {valueMax}
+      </p>
+    )}
+
+    {initialValue <= valueMax * -1 && (
+      <p style={{color: "blue", fontWeight: "bold"}}>
+        Atenção: Você atingiu o limite mínimo de {valueMax * -1}!
+      </p>
+    )}
+
+
     <br />
     <input 
     type="number"
     value={initialValue} 
-    onChange={(e) => {setvalueMax(e.target.value)}}
+    onChange={(e) => {setvalueMax(Number(e.target.value))}}
     
      />
-    </>
+    </div>
   )
 }
-export default App
+export default App;
