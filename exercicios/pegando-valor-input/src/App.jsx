4ae20e584f0cdb586d93e9cc0e75be491c4dc685
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-12-27 10:28:34 -0500

    feat: subindo minha resolução do exercício: 'pegando o valor do input'

--- exercicios/pegando-valor-input/src/App.jsx
+++ exercicios/pegando-valor-input/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
- 
+  const [inputValue, setInputValue] = useState("");
+  const [valueSubmit, setValueSubmit] = useState();
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setValueSubmit(inputValue);
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form action="" onSubmit={formSubmit}>
+      <input type="text" 
+      value={inputValue}
+      placeholder='Digite aqui' 
+      onChange={(e) => setInputValue(e.target.value)} />
+      <button type="submit">Enviar</button>
+  
+      <p style={{color: 'darkgrey', fontFamily: "sans-serif"}}>
+      O valor do input é: {valueSubmit}
+    </p>
+
+      {}
+    </form>
     </>
   )
 }
-
-export default App
+export default App;
