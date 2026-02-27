6aae8bfdf3c857da8285fb28b5e4b648e70a7928
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-02-27 10:45:38 -0500

    feat: exercitando largura, altura, flexbox e responsividade no Tailwind

--- todolist-app/src/App.tsx
+++ todolist-app/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
 
   return (
-    <>
-    <p className="text-[#6528d3] text-2xl text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem distinctio consequuntur mollitia est autem explicabo ex architecto eos, illo quod, ut velit voluptatum minus natus quisquam quos fugiat numquam dolor.</p>
-    </>
+    <div className="bg-orange-300 w-[500px] h-[500px] flex justify-between items-center lg:bg-violet-700">
+    <div className="bg-blue-500 w-40 h-40 m-4 sm:bg-black"></div>
+    <div className="bg-red-500 w-40 h-40 m-4 md:bg-green-400 min-[500px]:bg-amber-500"></div>
+    </div>
   )
 }
 
