322871848a7083bfb12e29122f644ad5e839d330
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-02-27 09:56:50 -0500

    feat: instalação de tailwind na pasta todolist-app, preparando projeto...

---
+++ todolist-app/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+    <p className="text-[#6528d3] text-2xl text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem distinctio consequuntur mollitia est autem explicabo ex architecto eos, illo quod, ut velit voluptatum minus natus quisquam quos fugiat numquam dolor.</p>
+    </>
+  )
+}
+
+export default App
