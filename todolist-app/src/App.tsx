c480f20641d6f99e58e906de0be38baff7215d57
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-01 16:23:16 -0500

    style: estilização completa do header

--- todolist-app/src/App.tsx
+++ todolist-app/src/App.tsx
@@ -1,16 +1,16 @@
 import TodoHeader from "./components/TodoHeader"
+import TodoForm from "./components/TodoForm"
 
 function App() {
 
   return (
    <main className="bg-neutral-very-dark-blue h-screen">
     <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+    <div className="max-w-[43.75rem] m-auto p-2">
     <TodoHeader></TodoHeader>
 
-    <form>
-      <span></span>
-      <input type="text" placeholder="Create a new todo. . ." name="todo" />
-    </form>
+    <TodoForm></TodoForm>
+    </div>
     </div>
    </main>
   )
