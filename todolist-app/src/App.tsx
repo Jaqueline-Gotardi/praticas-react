8bfe54617d14bd5ec6fcec07d9b2024bf24a2649
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-01 16:03:27 -0500

    feat: estruturando header

--- todolist-app/src/App.tsx
+++ todolist-app/src/App.tsx
@@ -1,7 +1,18 @@
+import TodoHeader from "./components/TodoHeader"
+
 function App() {
 
   return (
-   <p className="text-neutral-very-dark-grayish-blue-alt">Hello</p>
+   <main className="bg-neutral-very-dark-blue h-screen">
+    <div className="bg-[url('/images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+    <TodoHeader></TodoHeader>
+
+    <form>
+      <span></span>
+      <input type="text" placeholder="Create a new todo. . ." name="todo" />
+    </form>
+    </div>
+   </main>
   )
 }
 
