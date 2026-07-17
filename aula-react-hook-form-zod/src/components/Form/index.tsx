aa970102ebbfb83641f0ab54d9892373bbff6e8f
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-07-17 13:36:35 -0500

    feat: criar componente de formulário com estilos CSS

--- aula-react-hook-form-zod/src/components/Form/index.tsx
+++ aula-react-hook-form-zod/src/components/Form/index.tsx
@@ -1,1 +1,14 @@
-export
+export const Form = () => {
+    return (
+        <form>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" placeholder="Informe seu e-mail"/>
+
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Informe sua senha"/>
+
+            <label htmlFor="confirmPassword">Confirmar Senha</label>
+            <input type="password" id="confirmPassword" placeholder="Informe sua senha novamente"/>
+        </form>
+    )
+}
