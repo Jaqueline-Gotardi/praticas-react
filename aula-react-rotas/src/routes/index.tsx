484515e31b46d9a5960eb8b1490f89b8952e7669
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-02 15:56:43 -0500

    feat: adicionar rota para exibir detalhes do produto

--- aula-react-rotas/src/routes/index.tsx
+++ aula-react-rotas/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Home } from "../components/pages/Home";
 import { About } from "../components/pages/About";
 import { Products } from "../components/pages/Products";
 import { products } from "../mocks/products"
+import { ProductDetail } from "../components/pages/ProductDetail";
 
 
 export const Router = () => {
@@ -15,6 +16,7 @@ export const Router = () => {
             <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products products={products} />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
             </Route>
         </Routes>
     )
