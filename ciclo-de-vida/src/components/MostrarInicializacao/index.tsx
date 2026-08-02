d1a660a9f47e89a969abab73afff8eb361d6d68b
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-02 17:54:44 -0500

    feat: adicionar useEffect ao componente MostrarInicializacao

--- ciclo-de-vida/src/components/MostrarInicializacao/index.tsx
+++ ciclo-de-vida/src/components/MostrarInicializacao/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const MostrarInicializacao = ({ mensagem } : {mensagem: string}) => {
     const [ estado, setEstado ] = useState("inicialização");
