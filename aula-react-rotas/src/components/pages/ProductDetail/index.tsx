8083ffeb9420cb45ab7e5b0c024b108bffe6edba
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-02 16:02:03 -0500

    feat: adicionar lógica para exibir detalhes do produto na página ProductDetail

--- aula-react-rotas/src/components/pages/ProductDetail/index.tsx
+++ aula-react-rotas/src/components/pages/ProductDetail/index.tsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom"
+import { products } from "../../../mocks/products"
+
 export const ProductDetail = () => {
     const { id } = useParams()
 
+    const productDetail = products.find(product => product.id === Number(id))
+
     return (
+        <>
         <h2>Detalhes do produto</h2>
+
+        <div>
+            <img src={productDetail?.image} alt={productDetail?.name} />
+
+            <p>{productDetail?.name}</p>
+            <p>R${productDetail?.price},00</p>
+        </div>
+        </>
     )
 }
