4f407070d1f49beeb85c891dba253bf93d3ad681
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-02 15:36:59 -0500

    feat: implementar componente de produtos com mapeamento e exibição de informações

---
+++ aula-react-rotas/src/components/pages/Products/index.tsx
@@ -0,0 +1,34 @@
+import './Products.css'
+import { Link } from 'react-router-dom'
+import type { Product } from '../../../mocks/products'
+
+interface ProductProps {
+    products: Product[]
+}
+
+export const Products = ({ products }: ProductProps) => {
+    <section className='products-container'>
+        <h2>Nossos Produtos</h2>
+
+        <div className='products-grid'>
+            {products.map((product) => (
+                <Link 
+                to={`/product/${product.id}`}
+                key={product.id}
+                className="product-card"
+                >
+
+                <img src={product.image} alt={product.name} />
+
+                <div className='product-info'>
+                    <h3>{product.name}</h3>
+                    <p className='product-price'>
+                        R$ {product.price},00
+                    </p>
+                    <button type='button'>Adicionar ao carrinho</button>
+                </div>
+                </Link>
+            ))}
+        </div>
+    </section>
+}
