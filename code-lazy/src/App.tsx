0588e3869c9de811b785b4d5ce8e92ac0d03917a
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-10 13:47:40 -0500

    feat: corrigir importação do Suspense e implementar lógica de abertura do modal

--- code-lazy/src/App.tsx
+++ code-lazy/src/App.tsx
@@ -1,14 +1,29 @@
-import { useState, lazy, Supense } from 'react' 
+
+// Suspense = forma oficial do React de lidar com carregamento assíncrono de componentes
+import { useState, lazy, Suspense } from 'react' 
 import './App.css'
 
 const Modal = lazy(() => import('./components/Modal'))
 
 function App() {
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(prev => !prev);
+
+  }
+
   return (
-    <>
-      
-    </>
+      <div>
+        <h1>Exemplo de lazy loading com Modal</h1>
+
+        <button onClick={() => toggleModal}>Abrir modal</button>
+
+        <Suspense fallback={<p>Carregando modal...</p>}>
+        {isModalOpen && <Modal />}
+        </Suspense>
+        </div>
   )
 }
 
