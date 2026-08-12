d2dcc0a3e1d0167529b19b6a4fe6710fba347644
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-12 17:19:48 -0500

    feat: mover a função fetchPosts para dentro do hook usePosts

--- aula-tanstack-query/src/hooks/usePosts.ts
+++ aula-tanstack-query/src/hooks/usePosts.ts
@@ -1,6 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import type { Post } from "../types/types";
 
+const fetchPosts = async (limit: number = 10): Promise<Post[]> => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    );
+
+    //se não funcionar...
+    if(!response.ok) throw new Error("Erro ao buscar os posts");
+
+
+    //se funcionar...
+    return response.json();
+}
+
 export function usePosts(limit: number) {
     return useQuery<Post[]>({
         queryKey: ["posts", limit],
