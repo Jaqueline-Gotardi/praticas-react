f9c84bd80cd069b920f6ccc3c2a86050f611dd7b
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-12 17:03:36 -0500

    feat: adicionar suporte ao React Query e configurar o QueryClient no aplicativo

--- aula-tanstack-query/src/main.tsx
+++ aula-tanstack-query/src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// responsável por gerenciar todas as queries da aplicação
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
