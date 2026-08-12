275268f0c3231474542199109e9dc2e1947aab37
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-12 17:33:41 -0500

    feat: remover estado não utilizado do componente App

--- aula-tanstack-query/src/App.tsx
+++ aula-tanstack-query/src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import { Posts } from './components/Posts'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Posts />
     </>
   )
 }
