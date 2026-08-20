89d61bc6f5de33846405be798adaf046c0746a6d
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-20 17:51:58 -0500

    feat: ajustar estrutura e estilo do formulário de login

--- exercicios/formularioLogin/src/components/loginForm.tsx
+++ exercicios/formularioLogin/src/components/loginForm.tsx
@@ -1,7 +1,7 @@
 export function LoginForm() {
 
     return (
-        <form className="flex justify-center flex-col gap-4 text-white bg-[#050404]">
+        <form className="max-w-xl flex flex-col gap-4 text-white bg-[#050404]">
             <h1>Entrar</h1>
 
             <label htmlFor="email">Nome de usuário ou e-mail</label>
@@ -11,12 +11,14 @@ export function LoginForm() {
             <input type="password" name="password" id="password" placeholder="******" className="bg-[#130234] border-2 border-[#6528D3] rounded-md text-[#d5d0e6] py-4 px-3 w-full text-xs" />
 
             <div className="flex gap-2 items-center">
-            <input type="checkbox" className="w-4 h-4 border border-[#6528D3] rounded-sm cursor-pointer" /> <span className="">Manter conectado</span>
+            <input type="checkbox" className="hidden peer" />
+            <span className="w-4 h-4 border-2 border-[#6528D3] bg-black peer-checked:bg-[#6528D3] rounded-sm cursor-pointer"></span>
+             <span className="">Manter conectado</span>
             </div>
 
             <a className="text-[#6528D3] cursor-pointer">Recupere sua senha</a>
 
-            <button className="bg-[#6528D3] py-4 px-30 rounded-sm cursor-pointer">Entrar</button>
+            <button type="submit" className="bg-[#6528D3] py-4 px-30 rounded-sm cursor-pointer">Entrar</button>
 
         </form>
     )
