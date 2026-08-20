910ac189e10b7b498f234fb0f5d4b04474854fb3
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-20 17:40:00 -0500

    feat: ajustar estilo e estrutura do formulário de login

--- exercicios/formularioLogin/src/components/loginForm.tsx
+++ exercicios/formularioLogin/src/components/loginForm.tsx
@@ -1,7 +1,7 @@
 export function LoginForm() {
 
     return (
-        <form className="flex justify-center items-center flex-col gap-4 text-white text-2xl">
+        <form className="flex justify-center flex-col gap-4 text-white bg-[#050404]">
             <h1>Entrar</h1>
 
             <span>Nome de usuário ou e-mail</span>
@@ -10,7 +10,9 @@ export function LoginForm() {
             <span>Senha</span>
             <input placeholder="******" className="bg-[#130234] border-2 border-[#6528D3] rounded-md text-[#d5d0e6] py-4 px-3 w-full text-xs" />
 
-            <input type="checkbox" className="w-4 h-4 border-2 border-[#6528D3] rounded-sm cursor-pointer" /> <span>Manter conectado</span>
+            <div className="flex gap-2 items-center">
+            <input type="checkbox" className="w-4 h-4 border-2 border-[#6528D3] rounded-sm cursor-pointer" /> <span className="">Manter conectado</span>
+            </div>
 
             <a className="text-[#6528D3] cursor-pointer">Recupere sua senha</a>
 
