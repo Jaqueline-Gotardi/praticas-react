04212c90bf2a055d6eeafdd08fbb0d3f57c5fd4e
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-26 10:37:36 -0500

    feat: adicionar Tailwind CSS e remover componente não utilizado

--- exercicios/buscarDadosApi/src/App.tsx
+++ exercicios/buscarDadosApi/src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
-
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen bg-[#8690a6] flex items-center justify-center">
+      <h1 className="bg-[#1f2937] px-6 py-3 rounded-lg text-white font-bold">
+        Busque dados aleatórios!
+      </h1>
+    </div>
   )
 }
 
