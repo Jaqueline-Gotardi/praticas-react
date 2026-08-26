0303163e64ca54ac1f3d895ac3b488b1de565bde
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-26 11:34:26 -0500

    feat: implementar busca de produtos da API e atualizar estrutura do componente App

--- exercicios/buscarDadosApi/src/App.tsx
+++ exercicios/buscarDadosApi/src/App.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [mostrarProduto, setMostrarProduto] = useState();
+  
+  useEffect(() => {
+    const produtos = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setMostrarProduto(data);    
+    } catch (error) {
+      console.error("Erro ao buscar os produtos:", error)
+    }
+  }
+   produtos()
+}, [])
+
   return (
     <div className="min-h-screen bg-[#8690a6] flex items-center justify-center">
-      <h1 className="bg-[#1f2937] px-6 py-3 rounded-lg text-white font-bold">
+      <h1 className="text-4xl font-bold text-[#20091b]">
         Busque dados aleatórios!
       </h1>
-      
-      <div>
 
+      <div>
+        
       </div>
     </div>
   )
-}
+} 
 
 export default App
