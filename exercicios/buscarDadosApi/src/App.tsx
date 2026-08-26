37fb8d4e1dad0aea7e1c03397d59e1ff99d3291e
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-26 18:14:15 -0500

    style: melhorar a estrutura e formatação da renderização dos produtos

--- exercicios/buscarDadosApi/src/App.tsx
+++ exercicios/buscarDadosApi/src/App.tsx
@@ -27,19 +27,19 @@ function App() {
 }, [])
 
   return (
-    <div className="min-h-screen bg-[#8690a6] flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-[#8690a6] ">
       <h1 className="text-4xl font-bold text-[#20091b] mb-5">
         Produtos da Fake Store:
       </h1>
 
-      <div className="bg-[#888787] p-4 text-center ">
+      <div className="bg-[#888787] p-2 text-center grid grid-cols-2">
         {mostrarProduto.map((produto) => (
-          <div key={produto.id}>
-            <h2 className="text-[#0f0e0e] text-2xl">{produto.title}</h2>
-            <span>{produto.price}</span>
-            <p>{produto.description}</p>
-            <p>{produto.category}</p>
-            <img src={produto.image} alt={produto.title} />
+          <div key={produto.id} className="bg-[#0002] p-3 m-5 w-sm rounded-2xl">
+            <h2 className="text-[#0f0e0e] font-bold text-2xl mb-2">{produto.title}</h2>
+            <span className="text-green-950 font-bold text-2xl">{produto.price}</span>
+            <p className="font-sans m-2">{produto.description}</p>
+            <p className="bg-[#572b5744] rounded-2xl m-2">{produto.category}</p>
+            <img src={produto.image} alt={produto.title} className="w-xs " />
           </div>
         ))}
         
