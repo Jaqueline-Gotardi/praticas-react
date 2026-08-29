390ea03f4b31826087f30586a9b64114590ebba2
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-08-28 20:15:06 -0500

    style: ajustar formatação e espaçamento na renderização dos produtos

--- exercicios/buscarDadosApi/src/App.tsx
+++ exercicios/buscarDadosApi/src/App.tsx
@@ -28,18 +28,18 @@ function App() {
 
   return (
     <div className="min-h-screen bg-[#8690a6] ">
-      <h1 className="text-4xl font-bold text-[#20091b] mb-5">
+      <h1 className="text-4xl text-center p-5 font-bold text-[#20091b]">
         Produtos da Fake Store:
       </h1>
 
-      <div className="bg-[#888787] p-2 text-center grid grid-cols-2">
+      <div className="bg-[#888787] p-6 text-center grid grid-cols-2 justify-items-center gap-5">
         {mostrarProduto.map((produto) => (
-          <div key={produto.id} className="bg-[#0002] p-3 gap-5 w-sm rounded-2xl">
+          <div key={produto.id} className="bg-[#0002] p-3 w-sm rounded-2xl">
             <h2 className="text-[#0f0e0e] font-bold text-2xl mb-2">{produto.title}</h2>
             <span className="text-green-950 font-bold text-2xl">{produto.price}</span>
             <p className="font-sans m-2">{produto.description}</p>
             <p className="bg-[#572b5744] rounded-2xl m-2">{produto.category}</p>
-            <img src={produto.image} alt={produto.title} className="w-xs m-7 bg-gray-100" />
+            <img src={produto.image} alt={produto.title} className="w-xs m-7 bg-gray-100 rounded-3xl" />
           </div>
         ))}
         
