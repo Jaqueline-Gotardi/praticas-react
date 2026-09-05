dad73cb5596a2c55883787a844ee2324357a6872
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-09-05 14:01:22 -0500

    feat: adicionar formulário com campos para nome, preço e categoria do produto

--- exercicios/cadastroDeProdutos/src/App.tsx
+++ exercicios/cadastroDeProdutos/src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
 
   return (
-    <form>
+    <form className="flex flex-col gap-0.5 m-auto mt-10 w-sm "> 
+
+    <label htmlFor="nome do produto">Nome do Produto:</label>
+    <input type="text" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
+
+    <label htmlFor="nome do produto">Preço:</label>
+    <input type="text" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
+
+    <label htmlFor="nome do produto">Categoria:</label>
+    <input type="text" placeholder="Digite o nome do produto" className="border-gray-300 border-2" />
 
     </form>
   )
