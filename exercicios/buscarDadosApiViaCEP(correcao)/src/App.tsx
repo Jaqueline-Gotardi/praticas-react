f2af8b05d32764ea2aa7d5cb4b008f86c047a24b
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-09-04 21:51:26 -0500

    feat: implementar busca de CEP com validação e exibição de dados

--- exercicios/buscarDadosApiViaCEP(correcao)/src/App.tsx
+++ exercicios/buscarDadosApiViaCEP(correcao)/src/App.tsx
@@ -1,10 +1,84 @@
 import { useState } from 'react'
 
-function App() {
-
-  return (
-    
-  )
+interface Cep {
+    cep: string
+    logradouro: string
+    complemento: string
+    unidade: string
+    bairro: string
+    localidade: string
+    uf: string
+    estado: string
+    regiao: string
+    ibge: string
+    gia: string
+    ddd: string
+    siafi: string
 }
 
-export default App
+async function buscaCep(cep: string) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    return await response.json()
+}
+
+export const App = () => {
+    const [cepData, setCepData] = useState<Cep | null>(null)
+    const [error, setError] = useState<string | null>(null)
+
+
+    async function getCep(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        const formData = new FormData(event.currentTarget)
+        const cep = formData.get('cep') as string
+
+        setCepData(null)
+        setError(null)
+
+        if (cep.length !== 8) {
+            setError('CEP deve conter exatamente 8 dígitos')
+            return
+        }
+
+        try {
+            const cepData = await buscaCep(cep)
+
+            if (cepData.erro) {
+                setError('CEP não encontrado')
+                return
+            }
+
+            setCepData(cepData)
+        } catch (error) {
+            setError('Erro ao buscar CEP. Tente novamente.')
+            console.log(error)
+        }
+
+        event.currentTarget.reset()
+    }
+
+    return (
+        <form onSubmit={getCep}>
+            <label htmlFor='cep'>Digite seu CEP</label>
+            <input type='text' name='cep' id='cep' />
+            <button type='submit'>Buscar</button>
+
+            <div>
+                {error && (
+                    <div>
+                        <p>{error}</p>
+                    </div>
+                )}
+                {cepData && (
+                    <div>
+                        <p>CEP: {cepData?.cep}</p>
+                        <p>Logradouro: {cepData?.logradouro}</p>
+                        <p>Bairro: {cepData?.bairro}</p>
+                        <p>Cidade: {cepData?.localidade}</p>
+                        <p>Estado: {cepData?.uf}</p>
+                    </div>
+                )}
+            </div>
+        </form>
+    )
+}
