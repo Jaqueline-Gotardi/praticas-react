d72e309b2149af6d7b453d3e48e9cb37688f969a
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-09-05 14:34:09 -0500

    feat: corrigir estado inicial do produto e adicionar manipulação de envio no formulário

--- exercicios/cadastroDeProdutos/src/App.tsx
+++ exercicios/cadastroDeProdutos/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
 
-  const [produto, setProduto] = useState('');
+  const [produto, setProduto] = useState<Produto[]>([]);
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <form className="flex w-sm flex-col gap-0.5 bg-amber-50 p-5 shadow-md">
+      <form className="flex w-sm flex-col gap-0.5 bg-amber-50 p-5 shadow-md" onSubmit={handleSubmit}>
 
         <label htmlFor="produto">Nome do Produto:</label>
         <input type="text" name="produto" id="produto" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
@@ -38,7 +38,7 @@ function App() {
           <option value="livros">Livros e Ebooks</option>
         </select>
 
-        <button type="submit" onSubmit={handleSubmit} className="bg-blue-400 text-center rounded-4xl cursor-pointer">Cadastrar</button>
+        <button type="submit" className="bg-blue-400 text-center rounded-4xl cursor-pointer">Cadastrar</button>
 
       </form>
     </main>
