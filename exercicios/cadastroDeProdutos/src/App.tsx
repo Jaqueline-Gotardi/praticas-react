d2afadca97d45098569c6efcd90239f07da05ee8
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-09-05 14:20:44 -0500

    feat: adicionar IDs aos campos de entrada no formulário

--- exercicios/cadastroDeProdutos/src/App.tsx
+++ exercicios/cadastroDeProdutos/src/App.tsx
@@ -5,14 +5,14 @@ function App() {
       <form className="flex w-sm flex-col gap-0.5 bg-amber-50 p-5 shadow-md">
 
         <label htmlFor="produto">Nome do Produto:</label>
-        <input type="text" name="produto" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
+        <input type="text" name="produto" id="produto" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
 
         <label htmlFor="preco">Preço:</label>
-        <input type="text" name="preco" placeholder="Digite o valor" className="border-gray-300 border-2 mb-2.5" />
+        <input type="text" name="preco" id="produto" placeholder="Digite o valor" className="border-gray-300 border-2 mb-2.5" />
 
         <label htmlFor="categoria">Categoria:</label>
 
-        <select name="categoria" className="border-gray-300 border-2 mb-4">
+        <select name="categoria" id="categoria" className="border-gray-300 border-2 mb-4">
           <option value="">Selecione uma categoria</option>
           <option value="eletronicos">Eletrônicos</option>
           <option value="roupas">Roupas</option>
