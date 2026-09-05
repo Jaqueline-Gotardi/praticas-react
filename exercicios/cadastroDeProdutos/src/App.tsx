e468ed8960cdd5f6511efdb9e40e908037e648b8
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-09-05 14:12:51 -0500

    feat: ajustar estrutura do formulário para melhor responsividade

--- exercicios/cadastroDeProdutos/src/App.tsx
+++ exercicios/cadastroDeProdutos/src/App.tsx
@@ -1,27 +1,29 @@
 function App() {
 
   return (
-    <form className="flex flex-col gap-0.5 m-auto mt-10 w-sm "> 
+    <main className="flex min-h-screen items-center justify-center">
+      <form className="flex w-sm flex-col gap-0.5 bg-amber-50 p-5 shadow-md">
 
-    <label htmlFor="nome do produto">Nome do Produto:</label>
-    <input type="text" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
+        <label htmlFor="nome do produto">Nome do Produto:</label>
+        <input type="text" placeholder="Digite o nome do produto" className="border-gray-300 border-2 mb-2.5" />
 
-    <label htmlFor="nome do produto">Preço:</label>
-    <input type="text" placeholder="Digite o valor" className="border-gray-300 border-2 mb-2.5" />
+        <label htmlFor="nome do produto">Preço:</label>
+        <input type="text" placeholder="Digite o valor" className="border-gray-300 border-2 mb-2.5" />
 
-    <label htmlFor="nome do produto">Categoria:</label>
+        <label htmlFor="nome do produto">Categoria:</label>
 
-    <select className="border-gray-300 border-2">
-      <option value="">Selecione uma categoria</option>
-      <option value="eletronicos">Eletrônicos</option>
-      <option value="roupas">Roupas</option>
-      <option value="alimentos">Alimentos</option>
-      <option value="medicamentos">Medicamentos</option>
-      <option value="moveis">Móveis</option>
-      <option value="livros">Livros e Ebooks</option>
-    </select>
+        <select className="border-gray-300 border-2">
+          <option value="">Selecione uma categoria</option>
+          <option value="eletronicos">Eletrônicos</option>
+          <option value="roupas">Roupas</option>
+          <option value="alimentos">Alimentos</option>
+          <option value="medicamentos">Medicamentos</option>
+          <option value="moveis">Móveis</option>
+          <option value="livros">Livros e Ebooks</option>
+        </select>
 
-    </form>
+      </form>
+    </main>
   )
 }
 
